638f4fe9f07d3f242f30c4fabb7498d6e4f06297
SohamKottawar <kottawars10@gmail.com>
2023-10-02 22:25:19 +0530

    Alumni section added

---
+++ frontend/src/components/TeamPage/AlumniStatic.js
@@ -0,0 +1,129 @@
+const members=[
+    {
+        image: "https://media.licdn.com/dms/image/D4E03AQG5ErsPO1QO8g/profile-displayphoto-shrink_400_400/0/1695986630623?e=1701302400&v=beta&t=567NemlJIaJA-wAowZSW-ybCZYEgB8oWnweH_tHwjvM",
+        name: "Rameshwar Bhaskaran",
+        facebook: "https://www.facebook.com/rameshwarbhaskaran",
+        linkedin: "https://www.linkedin.com/in/rameshwarbhaskaran/"
+    },
+
+    {
+        image: "https://media.licdn.com/dms/image/C5603AQGp3wJDJZ22uA/profile-displayphoto-shrink_400_400/0/1597075568720?e=1701302400&v=beta&t=7fuOfRIMYQuJvVuWGuaWy0jckijSF5tyUvRKVexNpyA",
+        name: "Saumo Pal",
+        facebook: "https://www.facebook.com/saumo.pal.50",
+        linkedin: "https://www.linkedin.com/in/saumo-pal/"
+        /*Worked at Mercedes-Benz Research and Development India private Limited*/ 
+    },
+
+    {
+        image: "https://media.licdn.com/dms/image/D5603AQFK74Ki3h2GUQ/profile-displayphoto-shrink_400_400/0/1695361592304?e=1701302400&v=beta&t=1VIa6d7i7PMdewV6y5ELC74Dnvh3AAkCCQBrJ8cg_mc",
+        name: "Chinmay Sawkar",
+        facebook: "https://www.facebook.com/chinmay.sawkar",
+        linkedin: "https://www.linkedin.com/in/chinmay997/"
+        /*MS CS Grad @ Texas A&M | SDE Intern at Apple | Ex-Data Scientist at Fractal  */
+    },
+
+    {
+        image: "https://media.licdn.com/dms/image/C5103AQHgC0JeZnwuTA/profile-displayphoto-shrink_400_400/0/1574881857391?e=1701302400&v=beta&t=-BIMvVKvqMnQaO_BUIwPjTeFwtQ96Ws3sdFj-8JCiRs",
+        name: "Utkarsh Parasrampuria",
+        facebook: "https://www.facebook.com/utkarsh.parasarampuria",
+        linkedin: "https://www.linkedin.com/in/utkarsh-parasarampuria/"
+        /*Co-founder, Executive Director at Rawky Studios */
+    },
+
+    {
+        image: "https://media.licdn.com/dms/image/D5603AQEo9eyrLRHHcA/profile-displayphoto-shrink_400_400/0/1694944324650?e=1701302400&v=beta&t=Hv7D5qozPPq769iPgZ7jzdQdOrfMoZf6nOjkqKeMb64",
+        name: "Arnab Bir",
+        facebook: "https://www.facebook.com/arnab.bir.3",
+        linkedin: "https://www.linkedin.com/in/arnabbir/"
+        /*PhonePe | Ex-Google,Flipkart */
+    },
+
+    {
+        image: "https://media.licdn.com/dms/image/C5603AQG4qzUXA-MmjQ/profile-displayphoto-shrink_400_400/0/1517392946542?e=1701302400&v=beta&t=p64jg9RgTgLEnlFLo-GawLE4d6OT8_L4VqjkqRa5piM",
+        name: "Ashwary Anand",
+        facebook: "https://www.facebook.com/Ash.anand21",
+        linkedin: "https://www.linkedin.com/in/ashwary-anand-a83642107/"
+        /* Robotics Software Engineer in Koto China*/
+    },
+
+    {
+        image: "https://media.licdn.com/dms/image/D4D03AQGeK5fUnG0scA/profile-displayphoto-shrink_400_400/0/1685424361043?e=1701302400&v=beta&t=QfruKJwIY-X5x80QATCKydEELdfUq1SAJIhbdCDJzo4",
+        name: "Shivam Agarwal",
+        facebook: "https://www.facebook.com/agarwal.shivam207",
+        linkedin: "https://www.linkedin.com/in/sourav-choudhary-ab1464a0/"
+        /*PhonePe | Ex-Google,Flipkart */
+    },
+
+    {
+        image: "https://media.licdn.com/dms/image/D4D03AQGeK5fUnG0scA/profile-displayphoto-shrink_400_400/0/1685424361043?e=1701302400&v=beta&t=QfruKJwIY-X5x80QATCKydEELdfUq1SAJIhbdCDJzo4",
+        name: "Sourav Choudhary",
+        facebook: "https://www.facebook.com/sourav.choudhary.127",
+        linkedin: "https://www.linkedin.com/in/shivam-agarwal-543445186/"
+        /*Senior Data Scientist at C2FO */
+    },
+
+    {
+        image: "https://media.licdn.com/dms/image/C4E03AQEn0Dvyf-o3pw/profile-displayphoto-shrink_400_400/0/1595957759931?e=1701302400&v=beta&t=GQAOYHL8t-iELs1-Oo3HPaisAV0NV4yjk11Tv6JEwGU",
+        name: "Siddharth Singh",
+        facebook: "#",
+        linkedin: "https://www.linkedin.com/in/siddharth-singh-a7232a114/"
+        /*Doctorate Student at University of Maryland */
+    },
+
+    {
+        image: "https://media.licdn.com/dms/image/D4D03AQF6Gum-GJvnJA/profile-displayphoto-shrink_400_400/0/1671508298763?e=1701302400&v=beta&t=iDehL1GJobhZvXcVCSgTsmsJ9J869MQPHJPWpaPF9W8",
+        name: "Sirin Sanchay",
+        facebook: "#",
+        linkedin: "https://www.linkedin.com/in/sirin-sanchay-8b7a37256/"
+        /*Civil Servant || Indian Railways  */
+    },
+
+    {
+        image: "https://media.licdn.com/dms/image/C5103AQFMxdCeu7YA_w/profile-displayphoto-shrink_400_400/0/1517343612620?e=1701302400&v=beta&t=bfRihu_8tCplalw0ceUG6dgFTw4GvRu5JTPxjbDn20c",
+        name: "Naman Mitruka",
+        facebook: "https://www.facebook.com/nmitruka/about_work_and_education",
+        linkedin: "https://www.linkedin.com/in/namanmitruka/"
+        /*Product @ Vested | Peepul | Goldman Sachs */
+    },
+
+    {
+        image: "https://media.licdn.com/dms/image/C5103AQEit-quthpDOg/profile-displayphoto-shrink_400_400/0/1579510264505?e=1701302400&v=beta&t=WHrZyZxLzIZkAUDR6Az_H-V4WOzfitkDlwB5RCof9zE",
+        name: "Chinmay Gandhi",
+        facebook: "https://www.facebook.com/profile.php?id=100010597357642",
+        linkedin: "https://www.linkedin.com/in/gandhichinmay/"
+        /*Product Strategy & Ops @ Revolut | Ex - JPMC, ITC  */
+    },
+
+    {
+        image: "https://media.licdn.com/dms/image/C4E03AQFuJ6FluU-GQw/profile-displayphoto-shrink_400_400/0/1517467761815?e=1701302400&v=beta&t=dW1Xwq4kLNm9wxvVjF5rMcbylrgc6btDXEc_Y_VO9iw",
+        name: "Rathang Joshi",
+        facebook: "#",
+        linkedin: "https://www.linkedin.com/in/rathang-joshi-02a72891/"
+        /*PhonePe | Ex-Google,Flipkart */
+    },
+
+    {
+        image: "https://media.licdn.com/dms/image/D4D03AQF6Gum-GJvnJA/profile-displayphoto-shrink_400_400/0/1671508298763?e=1701302400&v=beta&t=iDehL1GJobhZvXcVCSgTsmsJ9J869MQPHJPWpaPF9W8",
+        name: "Ankush Chatterjee",
+        facebook: "#",
+        linkedin: "https://www.linkedin.com/in/ankushchatterjee/"
+        /*Working on Google Bard */
+    },
+
+    {
+        image: "https://media.licdn.com/dms/image/D4D03AQFG3P8Uib_M7g/profile-displayphoto-shrink_400_400/0/1695529236882?e=1701302400&v=beta&t=zM8YN7514OXwmtF70QaYcAnx1yF4XNIHxIAdV1tP_O0",
+        name: "Aakanksha Sangwan",
+        facebook: "#",
+        linkedin: "https://www.linkedin.com/in/sangwanaakanksha/"
+        /* Business Analytics professional */
+    },
+
+    {
+        image: "https://media.licdn.com/dms/image/D5603AQFIAaFLvDXb7Q/profile-displayphoto-shrink_400_400/0/1695009582232?e=1701302400&v=beta&t=jbu-a20ccRT8emTAcTKhci3-AN2XAXvoOBrWM_n4Qpk",
+        name: "Yash Lundia",
+        facebook: "https://www.facebook.com/yashlundia6495",
+        linkedin: "https://www.linkedin.com/in/yashlundia/"
+        /*Working in Microsoft */
+    },
+]
+export default members;
--- frontend/src/components/TeamPage/TeamPage.js
+++ frontend/src/components/TeamPage/TeamPage.js
@@ -1,10 +1,11 @@
 import React from "react"
 import TeamCard from "./TeamCard"
-import TeamCardSM from  "./TeamCardSM";
+import TeamCardSM from "./TeamCardSM";
 import TeamPageHeading from "./TeamPageHeading";
 import members from "./MembersStatic"
-import seniormembers from "./SeniorMembersStatic";
+// import seniormembers from "./SeniorMembersStatic";
 import advisors from "./AdvisorsStatic";
+import alumni from "./AlumniStatic";
 import Header from "./Header";
 import Fade from "react-reveal/Fade";
 import Particless from '../Common/Particles/Particless'
@@ -12,46 +13,80 @@ import Particless from '../Common/Particles/Particless'
 import "./TeamPage.css"
 
 const TeamPage = () => {
-    return  (
-        <>
-        <Header />
-        <Fade left>
+  return (
+    <>
+      <Header />
+      <Fade left>
         <TeamPageHeading text="Heads" />
-        </Fade>
-        <div className="members-head-list">
-            
+      </Fade>
+      <div className="members-head-list">
+
         {members?.map((member) => {
-            return <TeamCard key={member.id} member = {member} />;
-          }) 
+          return <TeamCard key={member.id} member={member} />;
+        })
         }
-        </div>
+      </div>
 
-        <Fade left>
+      <Fade left>
         <TeamPageHeading text="Advisors" />
-        </Fade>
-        <div className="members-head-list">
-            
+      </Fade>
+      <div className="members-head-list">
+
         {advisors?.map((member) => {
-            return <TeamCardSM key={member.id} member = {member} />;
-          }) 
+          return <TeamCardSM key={member.id} member={member} />;
+        })
         }
-        </div>
+      </div>
 
-        <br /><br /><br /><br /><br /><br /><br />
+      <br /><br /><br /><br /><br /><br /><br />
 
-        {/* <Fade left>
+      {/* <Fade left>
         <TeamPageHeading text="Senior Members" />
-        </Fade> */}
-        {/* <div className="members-head-list members-head-list-bottom">
+        </Fade> 
+        <div className="members-head-list ">   
             
         {seniormembers?.map((member) => {
             return <TeamCardSM key={member.id} member = {member} />;
           }) 
         }
-        </div> */}
-        <Particless />
-        </>
-    )
+
+        <Fade left>
+        <TeamPageHeading text="Alumni" />
+      </Fade>
+      <div className="members-head-list">
+
+        {alumni?.map((member) => {
+          return <TeamCardSM key={member.id} member={member} />;
+        })
+        }
+      </div>
+        </div>  */}
+
+{/* <Fade left>
+        <TeamPageHeading text="Senior Members" />
+      </Fade>
+      <div className="members-head-list">
+
+        {SeniorMembers?.map((member) => {
+          return <TeamCardSM key={member.id} member={member} />;
+        })
+        }
+      </div> */}
+
+      <Fade left>
+        <TeamPageHeading text="Alumni" />
+      </Fade>
+      <div className="members-head-list">
+
+        {alumni?.map((member) => {
+          return <TeamCardSM key={member.id} member={member} />;
+        })
+        }
+      </div>
+      <Particless />
+    </>
+  )
 }
 
 export default TeamPage;
+/*members-head-list-bottom  , I removed this from ,senior members, <div className="members-head-list members-head-list-bottom"> */
